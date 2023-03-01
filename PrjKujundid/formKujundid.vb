b0636625486bf9c29c5296b302dd494a35d17151
Vielder <iljagladius@gmail.com>
2023-03-01 16:43:15 +0200

    User expirience improvements

--- PrjKujundid/formKujundid.vb
+++ PrjKujundid/formKujundid.vb
@@ -8,34 +8,64 @@
     End Sub
 
     Private Sub btnKolmnurk_Click(sender As Object, e As EventArgs) Handles btnKolmnurk.Click
-        Dim objectKolmnurk As New Kolmnurk(txtKylgA.Text, txtKylgB.Text, ComboBox1.SelectedIndex)
+        'System.InvalidCastException' 
+        Dim objectKolmnurk As New Kolmnurk(0, 0, 0)
+        Try
+            objectKolmnurk = New Kolmnurk(txtKylgA.Text, txtKylgB.Text, ComboBox1.SelectedIndex)
+        Catch ex As Exception
+            MsgBox("Some of the values are invalid")
+        End Try
+
         tootleKujund(objectKolmnurk)
     End Sub
 
     Private Sub btnRuut_Click(sender As Object, e As EventArgs) Handles btnRuut.Click
-        Dim objektRuut As New Ruut(txtKylgA.Text, ComboBox1.SelectedIndex)
+        Dim objektRuut As New Ruut(0, 0)
+        Try
+            objektRuut = New Ruut(txtKylgA.Text, ComboBox1.SelectedIndex)
+        Catch ex As Exception
+            MsgBox("Some of the values are invalid")
+        End Try
         tootleKujund(objektRuut)
     End Sub
 
     Private Sub btnRistkulik_Click(sender As Object, e As EventArgs) Handles btnRistkulik.Click
-        tootleKujund(New Ristkylik(txtKylgA.Text, txtKylgB.Text, ComboBox1.SelectedIndex))
+        Try
+            tootleKujund(New Ristkylik(txtKylgA.Text, txtKylgB.Text, ComboBox1.SelectedIndex))
+        Catch ex As Exception
+            MsgBox("Some of the values are invalid")
+        End Try
     End Sub
 
     Private Sub Kujundid_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
+        txtYmbermoot.ReadOnly = True
+        txtPindala.ReadOnly = True
         ComboBox1.Items.Clear()
         ComboBox1.Items.Add("0")
         ComboBox1.Items.Add("1")
         ComboBox1.Items.Add("2")
         ComboBox1.Items.Add("3")
-        ComboBox1.Text = "Valige tapsus..."
+        ComboBox1.SelectedIndex = 0
     End Sub
 
     Private Sub btnRoopkulik_Click(sender As Object, e As EventArgs) Handles btnRoopkulik.Click
-        tootleKujund(New Roopkulik(txtKylgA.Text, txtKylgB.Text, txtKorgus.Text, ComboBox1.SelectedIndex))
+        Try
+            tootleKujund(New Roopkulik(txtKylgA.Text, txtKylgB.Text, txtKorgus.Text, ComboBox1.SelectedIndex))
+        Catch ex As Exception
+            MsgBox("Some of the values are invalid")
+        End Try
     End Sub
 
     Private Sub btnRomb_Click(sender As Object, e As EventArgs) Handles btnRomb.Click
-        tootleKujund(New Romb(txtKylgA.Text, txtKorgus.Text, ComboBox1.SelectedIndex))
+        Try
+            tootleKujund(New Romb(txtKylgA.Text, txtKorgus.Text, ComboBox1.SelectedIndex))
+        Catch ex As Exception
+            MsgBox("Some of the values are invalid")
+        End Try
+    End Sub
+
+    Private Sub txtYmbermoot_TextChanged(sender As Object, e As EventArgs) Handles txtYmbermoot.TextChanged
+
     End Sub
 End Class
